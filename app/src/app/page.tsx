23b7bdf304d3ac80e6a594b4f044d233c42d4c38
davisLjr <davisprojectjr@gmail.com>
2024-07-31 15:25:32 -0300

    se añade front temporal, en componentes se añade actualizacion de estilos de tipografia y sus variables, navegacion horizontal vertical y pestañas, y banner actualizado de lo ultimo de obelisco y por ultimo header

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,49 +1,9 @@
-import Link from "next/link";
+import HomeOne from "@/components/Home/home";
 
-export default function Home() {
+export default function Home () {
   return (
     <main>
-      {/* <button className="btn btn-primary">hola</button> */}
-
-      <h1 className="headline-lg">nunito</h1>
-      <p className="text-xs">open</p>
-
-      <div className="storybook__container-banner">
-        <div className="banner">
-          <div className="banner-content">
-            <p className="banner-text">
-              Esta descripción de banner <strong>puede tener énfasis</strong> y{" "}
-              <a href="#" target="_blank" rel="noreferrer">
-                enlace
-              </a>
-              .
-            </p>
-          </div>
-        </div>
-        <div className="banner banner-dark">
-          <div className="banner-content">
-            <p className="banner-text">
-              Esta descripción de banner <strong>puede tener énfasis</strong> y{" "}
-              <a href="#" target="_blank" rel="noreferrer">
-                enlace
-              </a>
-              .
-            </p>
-          </div>
-        </div>
-      </div>
-      <hr />
-      <div className="container">
-        <h4>Componentes</h4>
-
-        <Link href="/alert" className="d-block">Alerta</Link>
-        <Link href="/cards" className="d-block">Tarjetas</Link>
-        <Link href="/collapse" className="d-block">Colapsable</Link>
-        <Link href="/button" className="d-block">Botones</Link>
-        <Link href="/block" className="d-block">Bloque de trámite</Link>
-        <Link href="/badge" className="d-block">Etiquetas</Link>
-      </div>
-      
+      <HomeOne />
     </main>
   );
 }
